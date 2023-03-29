66b5b1ef49295871f39a4f3ff56ef92cab41fedf
ajith-ramanath <ajith.ramanath@gmail.com>
2023-03-29 09:30:11 +0100

    Changed the params

--- src/infra/aws-cli.sh
+++ src/infra/aws-cli.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 aws cloudformation create-stack \
---stack-name csa-stack-ajithr-4 \
+--stack-name csa-stack-ajithr-5 \
 --template-body file://./setup.yaml \
---parameters ParameterKey=StreamName,ParameterValue=csa-stream-3 \
+--parameters ParameterKey=StreamName,ParameterValue=csa-stream-4 \
              ParameterKey=ShardCount,ParameterValue=4 \
              ParameterKey=RetentionPeriodHours,ParameterValue=24
